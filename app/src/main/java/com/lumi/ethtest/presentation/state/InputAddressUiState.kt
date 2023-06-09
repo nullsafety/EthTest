f76f7ae58c48bf18f462a816df9eed00b5b69b84
Pavel Gosteev <p.gosteev@gmail.com>
2023-06-09 13:52:22 +0300

    Update states

--- app/src/main/java/com/lumi/ethtest/presentation/state/InputAddressUiState.kt
+++ app/src/main/java/com/lumi/ethtest/presentation/state/InputAddressUiState.kt
@@ -3,7 +3,7 @@ package com.lumi.ethtest.presentation.state
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 
-data class InputAddressUiState(
+class InputAddressUiState(
     val addressInput: MutableState<String> = mutableStateOf(""),
     val loadButtonEnabled: MutableState<Boolean> = mutableStateOf(false)
 )
--- app/src/main/java/com/lumi/ethtest/presentation/state/TransactionsUiState.kt
+++ app/src/main/java/com/lumi/ethtest/presentation/state/TransactionsUiState.kt
@@ -4,9 +4,8 @@ import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.lumi.ethtest.presentation.viewmodel.TransactionUIModel
 
-data class TransactionsUiState(
+class TransactionsUiState(
     val address: MutableState<String> = mutableStateOf(""),
-    val abi: MutableState<String> = mutableStateOf(""),
     val selectedTransaction: MutableState<TransactionUIModel?> = mutableStateOf(null),
     val loadingState: MutableState<LoadingState> = mutableStateOf(LoadingState.Loading),
     val transactions: MutableState<List<TransactionUIModel>> = mutableStateOf(listOf())
